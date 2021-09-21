f97c0f7007598af07cb3e8cda3c5301b07ce04d1
Boris <boria21@gmail.com>
2021-09-21 13:06:58 +0300

    datasets and models finished

--- datasets.py
+++ datasets.py
@@ -4,20 +4,25 @@ import torch
 from torch.utils.data import Dataset
 from PIL import Image
 import torchvision.transforms as transforms
+import random
 
 
 class DataSet1(Dataset):
-    def __init__(self, root, transforms_=None):
-        self.transform = transforms.Compose(transforms_)
-        # self.fnames = sorted(glob.glob(root + '/**/*'))
-        print()
+    def __init__(self, root, mode, transforms_=None):
+        self.transform = transforms_
+        self.fnames = sorted(glob.glob(root + '*.bmp'))
+        random.shuffle(self.fnames)
+        if mode == 'Train':
+            self.fnames = self.fnames[:-10]
+        elif mode == 'Validation':
+            self.fnames = self.fnames[-10:]
+
+        # print()
 
     def __getitem__(self, index):
         f = self.fnames[index]
-        name = os.path.basename(os.path.dirname(f))
-        label = int(colors_dict[name])-1
         img = self.transform(Image.open(f))
-        return img, label
+        return img
 
     def __len__(self):
         return len(self.fnames)
--- train_conv_autoencoder.py
+++ train_conv_autoencoder.py
@@ -6,7 +6,8 @@ import torch.utils.data
 import torch.nn as nn
 from models import AutoEncoder
 from PIL import Image
-from datasets import DataSet
+from datasets import DataSet1
+import torchvision.transforms as transforms
 
 # This script is for training Auto encoder on unlabeled Images at BMP format
 
@@ -14,7 +15,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--resume-epoch', type=int, default=None, help='starting epoch')
 parser.add_argument('--n_epochs', type=int, default=15, help='number of epochs of training')
 parser.add_argument('--batch-size', type=int, default=16, help='size of the batches')
-parser.add_argument('--data', type=str, default=r'../dataset/extra-data/tagged', help='root directory of the dataset')
+parser.add_argument('--data', type=str, default=r'./dataSet/extra-data/tagged', help='root directory of the dataset')
 parser.add_argument('--cuda', action='store_true', help='use GPU computation')
 parser.add_argument('--root-chkps', type=str, default='./checkpoints', help='use GPU computation')
 args = parser.parse_args()
@@ -25,7 +26,19 @@ args.dtype_long = torch.cuda.LongTensor if torch.cuda.is_available() and args.cu
 if not os.path.isdir(args.root_chkps):
     os.mkdir(args.root_chkps)
 
-def trainClassifer():
 
-dataset = DataSet1
+def trainAutoEncoder():
+    pass
 
+
+transforms_train = transforms.ToTensor()
+transforms_test = transforms.ToTensor()
+
+dataset_train = DataSet1(args.data, mode='Train')
+dataset_valid = DataSet1(args.data, mode='Validation')
+
+# define training and validation data loaders
+data_loader = torch.utils.data.DataLoader(
+    dataset_train, batch_size=args.batch_size, shuffle=True, num_workers=0)
+data_loader_test = torch.utils.data.DataLoader(
+    dataset_valid, batch_size=1, shuffle=False, num_workers=0)
